ff165e9711ebc5368e12c864fc42dd7b10277bbd
Oleksii Levzhynskyi <oleksii.levzhynskyi@gmail.com>
2015-06-26 18:48:55 +0300

    Move simple tab code + specs to separate project.

---
+++ dist/simple-tabs.js
@@ -0,0 +1,74 @@
+;(function () {
+  'use strict';
+
+
+  angular.module('SimpleTabs')
+    .factory('sTabs', TabFactory)
+    .directive('sToggleTab', ToggleTabClass)
+    .directive('sTab', TabClass);
+
+  function TabFactory () {
+    var tabs = {};
+
+    function _apply(name, action) {
+      if (['show', 'hide', 'toggle'].indexOf(action) === -1) {
+        console.warn('RTabs: action [' + action + '] not implemented!');
+      } else {
+        if (tabs[name]) {
+          tabs[name].map(function (el) {
+            if (el[action]) { el[action](); }
+          });
+          return true;
+        }
+      }
+      return false;
+    }
+
+    return {
+      _tabs: tabs,
+
+      toggle: function (name) {
+        return _apply(name, 'toggle');
+      },
+
+      hide: function (name) {
+        return _apply(name, 'hide');
+      },
+
+      show: function (name) {
+        return _apply(name, 'show');
+      },
+
+      add: function (name, element) {
+        if (!tabs[name]) {
+          tabs[name] = [];
+        }
+
+        tabs[name].push(element);
+      }
+    };
+  }
+
+  function ToggleTabClass(sTabs) {
+    return {
+      restrict: 'A',
+      link: function(scope, element, attrs) {
+        element.on('click', function () {
+          RTabs.toggle(attrs.rToggleTab);
+        });
+      }
+    };
+  }
+
+  function TabClass(sTabs) {
+    return {
+      restrict: 'A',
+      link: function(scope, element, attrs) {
+        RTabs.add(attrs.rTab, element);
+        if (!attrs.hasOwnProperty('rTabExpanded')) {
+          RTabs.hide(attrs.rTab);
+        }
+      }
+    };
+  }
+}());
---
+++ test/simple-tabs-spec.js
@@ -0,0 +1,84 @@
+;(function() {
+  'use strict';
+
+  describe("Factory: sTabs", function() {
+    var sTabs;
+
+    beforeEach(inject(function($injector) {
+      sTabs = $injector.get('sTabs');
+    }));
+
+    it("should have proper api", function () {
+      expect(Object.keys(sTabs)).toEqual(['_tabs', 'toggle', 'hide', 'show', 'add']);
+    });
+
+    describe('#add', function () {
+      it("should register new tab", function () {
+        var element = {};
+        sTabs.add('tabName', element);
+        expect(sTabs._tabs).toEqual({tabName: [element]});
+      });
+
+      it("should register few tabs with the same name", function () {
+        var element = {},
+            otherElement = {};
+        sTabs.add('tabName', element);
+        sTabs.add('tabName', otherElement);
+        expect(sTabs._tabs).toEqual({tabName: [element, otherElement]});
+      });
+    });
+
+    describe('#hide', function () {
+      itShouldBehaveLikeAction('hide');
+    });
+
+    describe('#show', function () {
+      itShouldBehaveLikeAction('show');
+    });
+
+    describe('#toggle', function () {
+      itShouldBehaveLikeAction('toggle');
+    });
+
+    function itShouldBehaveLikeAction (action) {
+      var element,
+          otherElement;
+
+      it("should call '" + action + "' on proper tab", function () {
+        element = jasmine.createSpyObj('element', [action]);
+        otherElement = jasmine.createSpyObj('otherElement', [action]);
+        sTabs.add('tabName', element);
+        sTabs.add('anotherTabName', otherElement);
+        sTabs[action]('tabName');
+
+        expect(element[action]).toHaveBeenCalled();
+        expect(otherElement[action]).not.toHaveBeenCalled();
+      });
+
+      it("should call '" + action + "' on all related tabs", function () {
+        element = jasmine.createSpyObj('element', [action]);
+        otherElement = jasmine.createSpyObj('otherElement', [action]);
+        sTabs.add('tabName', element);
+        sTabs.add('tabName', otherElement);
+        sTabs[action]('tabName');
+
+        expect(element[action]).toHaveBeenCalled();
+        expect(otherElement[action]).toHaveBeenCalled();
+      });
+
+      it("should return 'true' if tab is present", function () {
+        element = jasmine.createSpyObj('element', [action]);
+        sTabs.add('tabName', element);
+        sTabs[action]('tabName');
+
+        expect(sTabs[action]('tabName')).toBeTruthy();
+      });
+
+      it("should return 'false' if tab is not present", function () {
+        sTabs.hide('tabName');
+
+        expect(sTabs[action]('tabName')).toBeFalsy();
+      });
+    }
+  });
+}());
